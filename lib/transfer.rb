a339f6f391f54da99f07a38013cc478b049e0dd7
Frida Casas <fridakahsas@yahoo.com>
2018-09-23 23:56:05 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver
+  attr_accessor :amount, :status 
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount 
+    @status = "pending"
+  end 
+  
+  def valid? 
+    if @sender.valid? && @receiver.valid? == true 
+      return true 
+    else 
+      false 
+    end 
+  end 
+  
+  def execute_transaction
+    if  @sender.valid? && self.status == "pending" && sender.balance - self.amount > 0 
+      sender.balance -= self.amount 
+      receiver.balance += self.amount
+      self.status = "complete"
+    else 
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+  
+def reverse_transfer
+    if self.status == "complete"
+      sender.balance += self.amount
+      receiver.balance -= self.amount 
+    @status = "reversed"
+  end 
+end 
+  
 end
